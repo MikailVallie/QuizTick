b858690d2eb747dbda8eee49e745a9e48def8d60
Aviwe Jaxa <222208546@mycput.ac.za>
2025-07-01 20:29:22 +0200

    Login button & Scene

---
+++ QuizTick/Assets/Login.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Login : MonoBehaviour
+{
+      public void LoginButton()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    
+
+}
